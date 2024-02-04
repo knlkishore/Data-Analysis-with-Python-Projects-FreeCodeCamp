999b95f13f6f4031b38a7a7b3a3df63107c0c7f5
Kunal Kishore <kunalk16@outlook.in>
2024-02-04 22:39:22 +0530

    dummay

---
+++ 1.Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def calculate(list):
+    calculations = {}
+
+    axis1 = np.zeros(6,3)
+    for i in range(3):
+        for j in range(3):
+
+
+    return calculations
+
+
+"""
+{
+  'mean': [axis1, axis2, flattened],
+  'variance': [axis1, axis2, flattened],
+  'standard deviation': [axis1, axis2, flattened],
+  'max': [axis1, axis2, flattened],
+  'min': [axis1, axis2, flattened],
+  'sum': [axis1, axis2, flattened]
+}
+"""
